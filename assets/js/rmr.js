4d05b07f5a96358860d2cddf875591699c0fb4ee
Niko Kovacevic <nikovacevic@gmail.com>
2019-10-06 23:17:02 -0600

    Structure, typography, and reusable components mostly complete; side menu complete; editorconfig

---
+++ assets/js/rmr.js
@@ -0,0 +1,49 @@
+(function(window, document) {
+
+  var mainNav = document.querySelector('#main-nav');
+  console.log(mainNav);
+
+  var mainNavToggles = document.querySelectorAll('.toggle-main-nav');
+  console.log(mainNavToggles);
+
+  mainNavToggles.forEach(function (element) {
+    console.log(element);
+
+    element.addEventListener('click', function() {
+      console.log('mainNavToggle click');
+      toggleClass(mainNav, 'open');
+    });
+
+    element.addEventListener('focusout', function() {
+      console.log('mainNavToggle focusout');
+      removeClass(mainNav, 'open');
+    });
+  });
+
+  function toggleClass(element, className) {
+    if (hasClass(element, className)) {
+      removeClass(element, className);
+    } else {
+      addClass(element, className);
+    }
+  }
+
+  // https://stackoverflow.com/questions/6787383/how-to-add-remove-a-class-in-javascript
+  function hasClass(element, className) {
+    return !!element.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+  }
+
+  // https://stackoverflow.com/questions/6787383/how-to-add-remove-a-class-in-javascript
+  function addClass(element, className) {
+    if (!hasClass(element,className)) element.className += " " + className;
+  }
+
+  // https://stackoverflow.com/questions/6787383/how-to-add-remove-a-class-in-javascript
+  function removeClass(element, className) {
+    if (hasClass(element,className)) {
+      var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+      element.className=element.className.replace(reg,' ');
+    }
+  }
+
+})(window, document);
